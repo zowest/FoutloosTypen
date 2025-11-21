43f0d84d18e31eaf75fbbe2167a2274b415a5b79
co2mott <matthijswietsma@gmail.com>
2025-11-21 09:22:29 +0100

    Data layer setup and database connection/Foundation setup

---
+++ FoutloosTypen.App/ViewModels/LessonViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using FoutloosTypen.Core.Interfaces.Services;
+
+
+namespace FoutloosTypen.ViewModels
+{
+    public partial class LessonViewModel : BaseViewModel
+    {
+        private readonly ILessonService _lessonService;
+
+        public ObservableCollection<Lesson> Lessons { get; set; }
+    }
+}
