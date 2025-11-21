30adbaaebc5782108f9a2a1498d2c7f1301a8efb
ZoeWester <zoewesterveld@outlook.com>
2025-11-21 10:05:32 +0100

    Refactor IService and IRepository. CourseService implementation.

--- FoutloosTypen.Core/Interfaces/Services/IService.cs
+++ FoutloosTypen.Core/Interfaces/Services/IService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FoutloosTypen.Core.Models;
 
 namespace FoutloosTypen.Core.Interfaces.Services
 {
-    internal class IService
+    public interface IService<T> where T : Model
     {
+        List<T> GetAll();
+        T? Get(int id);
+        T Add(T item);
+        T? Update(T item);
+        T? Delete(T item);
     }
 }
