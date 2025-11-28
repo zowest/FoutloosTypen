b5909d0ab8dba329e066d604bf02cc414176b45b
ZoeWester <zoewesterveld@outlook.com>
2025-11-28 09:22:06 +0100

    added xaml.cs

--- FoutloosTypen.App/Views/AssignmentView.xaml.cs
+++ FoutloosTypen.App/Views/AssignmentView.xaml.cs
@@ -1,9 +1,25 @@
+using FoutloosTypen.ViewModels;
+
 namespace FoutloosTypen.Views;
 
 public partial class AssignmentView : ContentPage
 {
-	public AssignmentView()
+    private readonly AssignmentViewModel? _vm;
+    public AssignmentView()
 	{
 		InitializeComponent();
 	}
-}
+
+        public AssignmentView(AssignmentViewModel vm) : this()
+    {
+        BindingContext = _vm = vm;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (_vm is not null)
+            await _vm.OnAppearingAsync();
+    }
+
+    }
