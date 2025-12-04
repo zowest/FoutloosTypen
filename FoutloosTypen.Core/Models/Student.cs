df2fff99fcb194ac5a05eb83565fb0890cb8db1e
co2mott <matthijswietsma@gmail.com>
2025-12-04 08:56:50 +0100

    created model

---
+++ FoutloosTypen.Core/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoutloosTypen.Core.Models
+{
+    public partial class Student : Model
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Level { get; set; }
+        public double AvgSpeed { get; set; }
+        public double AvgPrecision { get; set; }
+
+        public Student(int id, string name, string password, int level, double avgSpeed, double avgPrecision) : base(id)
+        {
+            Name = name;
+            Password = password;
+            Level = level;
+            AvgSpeed = avgSpeed;
+            AvgPrecision = avgPrecision;
+        }
+    }
+}
