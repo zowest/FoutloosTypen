f6be6c99ad35dd6bc45040c89c69d34c28059897
co2mott <matthijswietsma@gmail.com>
2025-12-05 13:58:40 +0100

    styling changed

--- FoutloosTypen.App/Views/LoginView.xaml.cs
+++ FoutloosTypen.App/Views/LoginView.xaml.cs
@@ -8,5 +8,21 @@ public partial class LoginView : ContentPage
     {
         InitializeComponent();
         BindingContext = viewModel;
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+        {
+#if ANDROID
+            handler.PlatformView.BackgroundTintList =
+                Android.Content.Res.ColorStateList.ValueOf(
+                    Android.Graphics.Color.Transparent);
+#elif IOS || MACCATALYST
+            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+            handler.PlatformView.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
+            handler.PlatformView.Style = null;
+            handler.PlatformView.FocusVisualMargin = new Microsoft.UI.Xaml.Thickness(0);
+            handler.PlatformView.UseSystemFocusVisuals = false;
+#endif
+        });
     }
 }
