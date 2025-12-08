6b7fe9aa2e0cb7b3d777358d2de77034fec9ca25
Chenyu Xie <S1202909@student.windesheim.nl>
2025-12-08 16:04:39 +0100

    Added Id's to the Unit tests

--- TestCore/TestHashing.cs
+++ TestCore/TestHashing.cs
@@ -10,7 +10,7 @@ namespace TestCore
         {
         }
 
-        // Happy flow
+        // UT17-01: Correct wachtwoord matcht hash
         [Test]
         public void TestPasswordHashReturnsTrue()
         {
@@ -19,22 +19,23 @@ namespace TestCore
             Assert.That(PasswordHelper.VerifyPassword(password, passwordHash), Is.True);
         }
 
+        // UT17-02: Correct wachtwoord + testcase matcht hash
         [TestCase("boltype", "bHbXpFGYmI/YjrZvIVvu0Q==.kf5UUcO9kF5t9hSplOjbDUX2u2vle52Y4FHj4cFgE+s=")]
         public void TestPasswordHashReturnsTrueWithTestCase(string password, string passwordHash)
         {
             Assert.That(PasswordHelper.VerifyPassword(password, passwordHash), Is.True);
         }
 
-        // Unhappy flow
+        // UT17-03: Verkeerd wachtwoord faalt
         [Test]
         public void TestPasswordHashReturnsFalse()
         {
-            // correct hash maar verkeerd wachtwoord
             string wrongPassword = "verkeerdeWachtwoord";
             string correctHash = "bHbXpFGYmI/YjrZvIVvu0Q==.kf5UUcO9kF5t9hSplOjbDUX2u2vle52Y4FHj4cFgE+s=";
             Assert.That(PasswordHelper.VerifyPassword(wrongPassword, correctHash), Is.False);
         }
 
+        // UT17-04: Ongeldige hash gooit exception
         [TestCase("boltype", "bHbXpFGYmI/YjrZvIVvu0Q==.kf5UUcO9kF5t9hSplOjbDUX2u2vle52Y4FHj4cFgE+s")]
         public void TestPasswordHashThrowsOnInvalidHash(string password, string passwordHash)
         {
