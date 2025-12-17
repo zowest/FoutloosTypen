d02c33faa54da29e2048de5c93be77c935647a37
Meesvz123 <s1213648@student.windesheim.nl>
2025-12-17 21:32:09 +0100

    Code clean up

--- FoutloosTypen.App/ViewModels/LessonViewModel.cs
+++ FoutloosTypen.App/ViewModels/LessonViewModel.cs
@@ -1,11 +1,8 @@
 ﻿using System.Collections.ObjectModel;
-using FoutloosTypen.Core.Models;
-using FoutloosTypen.Core.Interfaces.Services;
 using System.ComponentModel;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Diagnostics;
 using CommunityToolkit.Mvvm.Input;
+using FoutloosTypen.Core.Interfaces.Services;
+using FoutloosTypen.Core.Models;
 
 namespace FoutloosTypen.ViewModels
 {
@@ -33,9 +30,7 @@ namespace FoutloosTypen.ViewModels
 
         public async Task LoadLessonsForCourseAsync(int courseId)
         {
-
             var allLessons = _lessonService.GetAll();
-
             var filteredLessons = allLessons
                 .Where(l => l.CourseId == courseId)
                 .OrderByDescending(l => l.Id)
@@ -43,19 +38,14 @@ namespace FoutloosTypen.ViewModels
 
             Lessons.Clear();
             foreach (var lesson in filteredLessons)
-            {
                 Lessons.Add(lesson);
-            }
 
             if (Lessons.Any())
                 SelectedLesson = Lessons.Last();
         }
 
         [RelayCommand]
-        private void SelectLesson(Lesson lesson)
-        {
-            SelectedLesson = lesson;
-        }
+        private void SelectLesson(Lesson lesson) => SelectedLesson = lesson;
 
         protected void OnPropertyChanged(string propertyName)
         {
