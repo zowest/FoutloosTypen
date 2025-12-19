5788f1de3c12a4bea213766e1c479991366dde53
co2mott <matthijswietsma@gmail.com>
2025-12-19 13:37:10 +0100

    fixed student.cs modle so that it works again

--- FoutloosTypen.Core/Models/Student.cs
+++ FoutloosTypen.Core/Models/Student.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FoutloosTypen.Core.Models
+﻿namespace FoutloosTypen.Core.Models
 {
     public partial class Student : Model
     {
@@ -15,13 +9,21 @@ namespace FoutloosTypen.Core.Models
         public double AvgSpeed { get; set; }
         public double AvgPrecision { get; set; }
         public bool UseTtsMode { get; set; }
-
-        public Student(int id, string username,  string name, string password, int level, double avgSpeed, double avgPrecision, bool useTtsMode = false)
         public int CompletedLessons { get; set; }
         public int TotalScore { get; set; }
 
-        public Student(int id, string username, string name, string password, int level, double avgSpeed, double avgPrecision, int completedLessons = 0, int totalScore = 0)
-            : base(id)
+        public Student(
+            int id,
+            string username,
+            string name,
+            string password,
+            int level,
+            double avgSpeed,
+            double avgPrecision,
+            bool useTtsMode = false,
+            int completedLessons = 0,
+            int totalScore = 0
+        ) : base(id)
         {
             Username = username;
             Name = name;
