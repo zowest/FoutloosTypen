b1b11925b6294bc68f004a536b97eca5f06f6bd7
Chenyu Xie <S1202909@student.windesheim.nl>
2025-12-19 14:29:27 +0100

    Revert "Reset before conflicts"
    This reverts commit 1663be8c809ffe15018b32aee2ee2d9b2bda3685.

--- FoutloosTypen.Core/Models/Student.cs
+++ FoutloosTypen.Core/Models/Student.cs
@@ -15,8 +15,6 @@ namespace FoutloosTypen.Core.Models
         public double AvgSpeed { get; set; }
         public double AvgPrecision { get; set; }
         public bool UseTtsMode { get; set; }
-        public int CompletedLessons { get; set; }
-        public int TotalScore { get; set; }
 
         public Student(int id, string username,  string name, string password, int level, double avgSpeed, double avgPrecision, bool useTtsMode = false)
             : base(id)
@@ -28,22 +26,6 @@ namespace FoutloosTypen.Core.Models
             AvgSpeed = avgSpeed;
             AvgPrecision = avgPrecision;
             UseTtsMode = useTtsMode;
-        }
-
-        public Student(int id, string username, string name, string password, int level,
-                       double avgSpeed, double avgPrecision, bool useTtsMode,
-                       int completedLessons, int totalScore)
-            : base(id)
-        {
-            Username = username;
-            Name = name;
-            Password = password;
-            Level = level;
-            AvgSpeed = avgSpeed;
-            AvgPrecision = avgPrecision;
-            UseTtsMode = useTtsMode;
-            CompletedLessons = completedLessons;
-            TotalScore = totalScore;
         }
     }
 }
